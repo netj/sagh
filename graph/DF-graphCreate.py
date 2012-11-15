3e50f5a5dd9bf724bf3bd1a56974636a97b7d757
Jaeho Shin <netj@sparcs.org>
2012-11-15 05:23:51 -0800

    compute common-files, DF-graph

---
+++ graph/DF-graphCreate.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+'''
+Input: Commit Log File from standard input
+Output: Pickled bipartite developers-files graph in DF-graph.txt
+'''
+
+import networkx as nx
+from collections import defaultdict
+import pickle
+import sys
+
+DFbipartite = nx.Graph()
+flag = False
+count  = 1
+
+author_id = '';
+for line in sys.stdin:
+    
+    if (flag==True and line=="\n"):
+            count-=1
+            continue
+            
+    if ('files changed' in line or 'file changed' in line): 
+        count=1
+        flag = False
+        continue
+    
+    if (count==0):
+        file_name = line[0:line.find('|')].strip()
+        #print file_name
+        DFbipartite.add_node(file_name, type='File')
+        DFbipartite.add_edge(author_id,file_name)
+        continue
+        
+    if 'Author:' in line:
+        flag = True
+        author_id = line[len('Author: '):].strip()
+        
+        #print
+        #print author_id, author_name
+        DFbipartite.add_node(author_id, type='Developer')
+    
+pickle.dump(DFbipartite, open('DF-graph.txt', 'w'))
+
+graph = DFbipartite
+score = defaultdict(int)
+lDeveloper = []
+
+for files in graph.nodes(data=True):
+    
+    if(files[1]['type']!='File'): continue
+    lDeveloper = graph.neighbors(files[0])
+    
+    for i in range(len(lDeveloper)):
+        for j in range(i+1,len(lDeveloper)):
+            score[(lDeveloper[i],lDeveloper[j])] += 1
+            score[(lDeveloper[j],lDeveloper[i])] += 1
+  
+lDeveloper = []  
+for developer in graph.nodes(data=True):
+    if(developer[1]['type']!='Developer'): continue
+    lDeveloper.append(developer[0])
+
+#Output the scores of the graph
+#f = open('DD-score.txt', 'w')
+
+for i in range(len(lDeveloper)):
+    for j in range(i+1,len(lDeveloper)):
+        print lDeveloper[i]+'\t'+lDeveloper[j]+'\t'+str(score[(lDeveloper[i],lDeveloper[j])])
+        #f.write(lDeveloper[i]+'\t'+lDeveloper[j]+'\t'+str(score[(lDeveloper[i],lDeveloper[j])])+'\n')
+
+#f.close()
